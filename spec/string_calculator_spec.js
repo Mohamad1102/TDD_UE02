796b1e2473505c31d5ca60e7f148e392f1654bd1
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-06-02 17:52:02 +0200

    test cases were written (AISSA)

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -6,4 +6,21 @@ describe('String Calculator', function() {
 	it('should return 0 for an empty string', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
+
+	it('should return 6 for input [1, 2, 3, 4, 5]', function() {
+		expect(electronsAroundCores([1, 2, 3, 4, 5])).toEqual(6);
+	});
+	
+	it('should return 4 for input [2, 2, 3, 3]', function() {
+		expect(electronsAroundCores([2, 2, 3, 3])).toEqual(4);
+	});
+	
+	it('should return 2 for input [6, 6, 4, 4, 1, 3]', function() {
+		expect(electronsAroundCores([6, 6, 4, 4, 1, 3])).toEqual(2);
+	});
+	
+	it('should return 12 for input [3, 5, 3, 5, 4, 2]', function() {
+		expect(electronsAroundCores([3, 5, 3, 5, 4, 2])).toEqual(12);
+	});
+	
 });
