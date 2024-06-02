641607a944205d02f90bf5de0d54bc1db06d7b91
Mohamad1102 <152527808+Mohamad1102@users.noreply.github.com>
2024-06-02 18:15:44 +0200

    simplest production code has been written (MOSALLEM)

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,20 @@
 // StringCalculator.js
 function StringCalculator() {}
+
 StringCalculator.prototype.add = function(string_numbers) {
-	//
+    const numbers = string_numbers.split(',').map(Number);
+    return electronsAroundCores(numbers);
 };
+
+function electronsAroundCores(diceArray) {
+    // Sort the array in ascending order
+    diceArray.sort((a, b) => a - b);
+    
+    // Calculate the sum of the last five elements in the sorted array
+    let sum = 0;
+    for (let i = diceArray.length - 1; i >= diceArray.length - 5; i--) {
+        sum += diceArray[i];
+    }
+    
+    return sum;
+}
